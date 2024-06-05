c03b0b23250ef7f7a61a92d927f9028a47d3c353
Xelem <anselmchizurum@gmail.com>
2024-06-05 18:15:26 +0100

    Fixed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,22 @@ import Navbar from './Navbar'
 
 const Hero = () => {
     return (
-        <div className="bg-custom-bg bg-cover bg-center bg-no-repeat h-screen">
+        <div className="bg-custom-bg bg-cover bg-center bg-no-repeat relative h-screen">
             <Navbar />
             <div
                 id="hero"
-                className="p-12 text-center flex flex-col justify-center items-center"
+                className="p-12 text-center flex flex-col justify-center items-center bg-[#231f20] bg-opacity-70 h-[70%] md:w-[40%] mx-auto text-white absolute left-0 right-0 top-[55%] translate-y-[-50%] rounded-3xl"
             >
-                <h1 className="text-7xl hero-text">
-                    Nacho The <span className="text-[#65c0b1]"> 𐤊at</span>
-                </h1>
-                <p className="py-4">
-                    The perfect example of fairness and the true spirit of Kaspa
-                    and KRC20!
+                <img
+                    src="kaspa_logo_nacho_512.png"
+                    alt="Nacho"
+                    className="h-48 w-48"
+                />
+
+                <p className="font-bold text-3xl my-4">Nacho the 𐤊at</p>
+                <p className="font-bold text-2xl">
+                    Inspired by Shai Wyborski's pet cat Nacho
                 </p>
-                <p className="">Inspired by Shai Wyborski's pet cat Nacho</p>
             </div>
         </div>
     )
